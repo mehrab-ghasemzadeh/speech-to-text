5f27e4bc5cb67e8c29a1671de67942e0e13c165c
mehrab-ghasemzadeh <mehrab.ghw@gmail.com>
2025-01-06 15:06:09 +0330

    the code fixed for low memory PCs.

--- speech_to_text/dataSetProcessing.py
+++ speech_to_text/dataSetProcessing.py
@@ -1,38 +1,54 @@
-import pyarrow.parquet as pq
-# import pandas as pd
+import pandas as pd
 from os import listdir
-from os.path import join
+from os.path import join, isfile
 import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 import io
+from memory_profiler import memory_usage
 
 def plot_spectrogram(Y, sr, hop_length, y_axis="log", fileName = '_'):
+  try:
     plt.figure(figsize=(25, 10))
     librosa.display.specshow(Y, sr=sr, hop_length=hop_length, x_axis="time", y_axis=y_axis)
     plt.colorbar(format="%+2.f")
-    plt.savefig(join('./dataSetSpectogramOutputs', f"{fileName}.png"))
+    plt.savefig(join('./speech_to_text\spectogramOutputs', f"{fileName}.png"))
+    plt.close('all')
+    plt.cla()
+    plt.clf()
+  except:
+    print(f"couldn't create spectogram with the voice : {fileName}")
 
-def create_spectogram(filename: str) -> None:
-  df = pq.read_table(source=filename).to_pandas() or None
-  for audio,clientId in zip(df.audio, df.client_id):
+def create_spectogram(voice, name):
     try:
-      file, sr = librosa.load(io.BytesIO(audio['bytes']))
+      file , sr = librosa.load(io.BytesIO(voice['bytes']))
       FRAME_SIZE = 2048
       HOP_SIZE = 512
       s_file = librosa.stft(file, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
       Y_file = np.abs(s_file) ** 2
       Y_log_file = librosa.power_to_db(Y_file)
-      plot_spectrogram(Y_log_file, sr, HOP_SIZE,'log', fileName= clientId)
+      plot_spectrogram(Y=Y_log_file, sr= sr, hop_length= HOP_SIZE, y_axis="log", fileName = name)
+      del file, sr
+      print(memory_usage())
     except:
-      print(f"error on voice : {clientId}")
-    del df
+      print(f"error on voice : {name}")
 
 if __name__ == "__main__":    
-  path = '.\Persian_Common_Voice_17_0\data'
+  path = 'speech_to_text\Persian_Common_Voice_17_0\data'
+  i=1
   for file in listdir(path):
     try:
-      create_spectogram(join(path,file))
+      df = pd.read_parquet(join(path,file))
+      audios = df.audio
+      clientIds = df.client_id
+      sentences = df.sentence
+      print(file)
+      del df, file
+      for audio, clientId, sentence in zip(audios, clientIds, sentences):
+        create_spectogram(audio, f"{i}-{clientId}")
+        print(i)
+        i+=1
+      del audios, clientIds, sentences
     except:
-      print(f'error on file : "{join(path,file)}"')
+      print(f"error on : {file}")
